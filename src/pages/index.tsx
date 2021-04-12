7a28938a4fa34069833ea581c52792829c39419b
Jr.Soares <juniorsoaresti@gmail.com>
2021-04-12 15:43:11 -0400

    Page signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {Flex} from '@chakra-ui/react'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -7,7 +7,59 @@ export default function Home() {
      h="100vh"
      justifyContent="center"
      alignItems="center">
-     <h1>Olá</h1>
+      <Flex
+         as="form"
+         width="100%"
+         maxWidth="360"
+         background="gray.800"
+         padding="8"
+         borderRadius="8"
+         flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              background="gray.900"
+              variant="filled"
+              _hover={{ 
+                backgroundColor: "gray.900"
+              }}
+              size="lg"
+              
+            />
+          </FormControl>
+          
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                focusBorderColor="pink.500"
+                background="gray.900"
+                variant="filled"
+                _hover={{ 
+                  backgroundColor: "gray.900"
+                }}
+                size="lg"
+              />
+          </FormControl>       
+        </Stack>
+
+        <Button
+            type="submit"
+            marginTop="6"
+            colorScheme="pink"
+            size="lg"
+        >
+            Entrar
+        </Button>
+       </Flex>
    </Flex>
   )
 }
