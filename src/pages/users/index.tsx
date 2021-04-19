892986fe14a991ce562e426f29fbbba03134792e
Jr.Soares <juniorsoaresti@gmail.com>
2021-04-19 15:56:04 -0400

    Structuring and styling the page: list of users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,159 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/Siderbar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+        <Box flex="1" borderRadius="8" background="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody></Tbody>
+            <Tr>
+              <Td px="6">
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Júnior Soares</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    juniorsoaresti@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              <Td>07 de Abril de 2021</Td>
+              <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px="6">
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Júnior Soares</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    juniorsoaresti@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              <Td>07 de Abril de 2021</Td>
+              <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px="6">
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Júnior Soares</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    juniorsoaresti@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              <Td>07 de Abril de 2021</Td>
+              <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td px="6">
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Júnior Soares</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    juniorsoaresti@gmail.com
+                  </Text>
+                </Box>
+              </Td>
+              <Td>07 de Abril de 2021</Td>
+              <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+              </Td>
+            </Tr>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
