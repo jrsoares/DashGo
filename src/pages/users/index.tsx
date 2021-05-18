a7ff81fdd5bafbf0c78f2c77d8220443adfb8b6f
Jr.Soares <juniorsoaresti@gmail.com>
2021-05-18 14:16:06 -0400

    creating the useUsers hook for listing users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,28 +22,10 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 import Link from "next/link";
-import { useQuery } from "react-query";
+import { useUsers } from "../../hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-    return users;
-    return users;
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
